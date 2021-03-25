72e2468f3a6168cfd993ea69acc29c45ca3f0ae4
Bernardo Pretto <bernardodgpretto@gmail.com>
2021-03-25 04:59:21 -0300

    Create specification and import category routes fixed, ocumented more features on /api-docs/

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,4 +1,4 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import { ISpecificationsRepository } from "../../repositories/ISpecificacionsRepository";
 
@@ -7,6 +7,7 @@ interface IRequest {
     description: string;
 }
 
+@injectable()
 class CreateSpecificationUseCase {
     constructor(
         @inject("SpecificationsRepository")
