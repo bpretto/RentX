5fadc66ee670ee38e97d349582de35250d76d359
Bernardo Pretto <bernardodgpretto@gmail.com>
2021-05-14 14:21:19 -0300

    detailing forgotpasswordmail

--- src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidv4 } from "uuid";
 
@@ -20,8 +21,17 @@ class SendForgotPasswordMailUseCase {
         private mailProvider: IMailProvider
     ) {}
 
-    async execute(email: string) {
+    async execute(email: string): Promise<void> {
         const user = await this.usersRepository.findByEmail(email);
+
+        const templatePath = resolve(
+            __dirname,
+            "..",
+            "..",
+            "views",
+            "emails",
+            "forgotPassword.hbs"
+        );
 
         if (!user) {
             throw new AppError("User does not exists!");
@@ -37,10 +47,16 @@ class SendForgotPasswordMailUseCase {
             expire_date,
         });
 
+        const variables = {
+            name: user.name,
+            link: `${process.env.FORGOT_PASSWORD_URL}${token}`,
+        };
+
         await this.mailProvider.sendMail(
             email,
             "Recuperação de senha",
-            `O link para o reset é: ${token}`
+            variables,
+            templatePath
         );
     }
 }
