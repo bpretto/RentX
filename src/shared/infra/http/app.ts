1361256700b7034b6be04ca73a6e6819637e0df1
Bernardo Pretto <bernardodgpretto@gmail.com>
2021-06-29 17:08:55 -0300

    implementing avatar's url, now users can see their own profile

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,6 +8,8 @@ import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 import "@shared/container";
 
+import upload from "@config/upload";
+
 import SwaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
@@ -17,6 +19,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(SwaggerFile));
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
