af2327519f1a6a7d7340f5138284421b76079830
Bernardo Pretto <bernardodgpretto@gmail.com>
2021-07-02 14:10:11 -0300

    preparing deploy

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
 import auth from "@config/auth";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -14,6 +15,7 @@ export async function ensureAuthenticated(
     next: NextFunction
 ): Promise<void> {
     const authHeader = request.headers.authorization;
+    const userTokensRepository = new UsersTokensRepository();
 
     if (!authHeader) {
         throw new AppError("Token missing!", 401);
@@ -21,7 +23,19 @@ export async function ensureAuthenticated(
     const [, token] = authHeader.split(" ");
 
     try {
-        const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
+        const { sub: user_id } = verify(
+            token,
+            auth.secret_refresh_token
+        ) as IPayload;
+
+        const user = await userTokensRepository.findByUserIdAndRefreshToken(
+            user_id,
+            token
+        );
+
+        if (!user) {
+            throw new AppError("User doesn't exists!", 401);
+        }
 
         request.user = {
             id: user_id,
