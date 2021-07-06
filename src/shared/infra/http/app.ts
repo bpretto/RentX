7d2a7e2f167c19f9d4308e7519cdf9800682c4f3
Bernardo Pretto <bernardodgpretto@gmail.com>
2021-07-06 19:16:06 -0300

    implementing sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,6 +2,10 @@ import "reflect-metadata";
 import "dotenv/config";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
+
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
@@ -19,6 +23,18 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(SwaggerFile));
@@ -26,8 +42,9 @@ app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
-
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
